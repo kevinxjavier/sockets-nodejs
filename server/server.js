95ce7c7f6e7231a1f2b10b03bc9a30f58b2b4c46
Kevin Pina <kpina.trebolit@bancochile.cl>
2019-10-08 17:35:32 -0300

    Socket Version2: Retroalimentacion de emisiones Cliente-Servidor y Servidor-Cliente

--- server/server.js
+++ server/server.js
@@ -17,12 +17,33 @@ app.use(express.static(publicPath));
 */
 let io = socketIO(server);
 
+/*
+    - En el HTML, se ejecuta socket.on('connect'), socket.on('disconnect') 
+        y socket.on('mensaje') así no se haya refrescado la página. "Hay 
+        que refrescar para que se ejecuten los socket.emit('mensaje')".
+
+    - En NodeJS, se ejecutan io.on('connection'), cliente.on('disconnect'),
+        cliente.emit('mensaje') y cliente.on('mensaje') si ya un cliente envía info.
+*/
 io.on('connection', (cliente) => {
     console.log('Cliente conectado...');
 
-    cliente.on('disconnect', ()=> {
+    // on() para Recibir Informacion
+    cliente.on('disconnect', () => {
         console.log('Cliente desconectado...');
     });
+
+    cliente.on('miMensaje1', (mensaje, callback) => {
+        console.log(mensaje);
+        callback('Server Respondiendo: se recibio mensaje del Cliente exitosamente...');
+    });
+
+    cliente.on('usuario', (mensaje) => {
+        console.log(mensaje);
+    });
+
+    // emmit() para Enviar Informacion
+    cliente.emit('usuario', {usuario: 'administrador', mensaje: 'Welcome'});
 })
 
 server.listen(port, (err) => {
