76089b807f16094677fb57e2b9ae272fda18bf32
Kevin Pina <kpina.trebolit@bancochile.cl>
2019-10-08 12:20:31 -0300

    Socket Version1: Configuracion Basica

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const app = express();
+let server = http.createServer(app);
+
+const publicPath = path.resolve(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+/* 
+    Esto lo usaremos para la comunicacion con el backend en el frontend index.html var socket = io(); 
+    estos objetos tanto el back como en el front es el mismo en contenido y nombre (o casi igual).
+*/
+let io = socketIO(server);
+
+io.on('connection', (cliente) => {
+    console.log('Cliente conectado...');
+
+    cliente.on('disconnect', ()=> {
+        console.log('Cliente desconectado...');
+    });
+})
+
+server.listen(port, (err) => {
+    if (err) 
+        throw new Error(err);
+    console.log(`Servidor corriendo en puerto ${ port }`);
+});
